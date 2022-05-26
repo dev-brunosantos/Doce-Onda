368a3b54e44fa6506d17d9ab661d88fcea19d7dd
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-05-26 13:02:27 -0300

    Atualização para dispositivos Mobile

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// MENU PRINCIPAL PARA PEQUENAS TELAS
+
+// CONFIGURAÇÕES DO MENU PRINCIPAL
+
+const menuPrincipal = document.querySelector('nav');
+
+function abrirMenu() {
+    menuPrincipal.style.display = "flex";
+}
+function fecharMenu() {
+    menuPrincipal.style.display = "none";
+}
